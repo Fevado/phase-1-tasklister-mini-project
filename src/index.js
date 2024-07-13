d9cab9c42a4222da27d45f6d25d5c84482df09c1
favoured mwange <mwangefavoured@gmail.com>
2024-07-13 16:44:33 +0300

    Adds DOM elements

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+const task=["Eat", "Feed Cat", "Play"]
+let comtainer= document.getElementById('tasks')
+
+ task.map((todo)=>{
+   let myTodo=document.createElement('li')
+   myTodo.textcontent= todo
+   comtainer.appendChild(myTodo)
+ })
+
+const taskInput= document.getElementById('new-task-description')
+taskInput.addEventListener('input', handleAdd)
+
+let taskName;
+function handleAdd(e){
+ taskName=e.target.value
+}
+
+
+const myForm= document.getElementById('create-task-form')
+myForm.addEventListener('submit', handleSubmit)
+
+function handleSubmit(e){
+  e.preventDefault()
+  task.push(taskName)
+}
